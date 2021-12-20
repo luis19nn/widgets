2c5ad69f2421ea6749fbd1bfcdfa97bee1062281
Luis Ferreira <luis19nn@gmail.com>
2021-12-20 13:54:43 -0300

    Começando a navegação sem React Router

--- src/App.js
+++ src/App.js
@@ -35,9 +35,29 @@ const options = [
 ];
 
 const App = () => {
+    const [selected, setSelected] = useState(options[0]);
+    const [showDropdown, setShowDropdown] = useState(true);
+
     return (
         <div className="ui container">
-            <Accordion items={items} />
+            <div className="ui container">
+                <button onClick={() => setShowDropdown(!showDropdown)}>
+                    Toggle Dropdown
+                </button>
+                {showDropdown ? (
+                    <Dropdown
+                        options={options}
+                        selected={selected}
+                        onSelectedChange={setSelected}
+                    />
+                ) : null}
+            </div>
+            <br />
+            <hr />
+            <br />
+            <div className="ui container">
+                <Translate />
+            </div>
         </div>
     );
 };
